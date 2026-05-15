2f9bfc23dcaf770e6d57491019923dec5a2d8630
Keerti Maddi <keertimaddi22@gmail.com>
2026-05-15 15:40:56 +0530

    spelling fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ import TeacherAssignments from "./Pages/TeacherDashboard/TeacherAssignments";
 
 import Attendance from "./Pages/Attendance/Attendance";
 
-import Sidebar from "./Components/Sidebar/Sidebar.jsx";
+import Sidebar from "./Components/SideBar/Sidebar";
 import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
 
 import Channels from "./Pages/Channels/Channels";
